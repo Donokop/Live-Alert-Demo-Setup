f5fa010e08fd503136ced8c6fd42b8e656de871f
Donokop <pjan05201@gmail.com>
2026-05-30 21:11:36 +0200

    Script tied to commit
    This script is now tied to the 'e901d3e' commit of Live Alert Demo so that the script does not depend on changes done in a different branch

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
-
 #!/bin/bash
 
 # Variables you can change to personalize install:
 PARENT_DIRECTORY=/IntelOpenEdge
 DIRECTORY_TO_SAVE_AT="$PARENT_DIRECTORY/edge-ai-suites"
+# Doesn't update by itself, ties to a known commit that works with the program
+# You can change the commit version or the 'git checkout' command in this script if you want a newer version
+# but it hasn't been tested there and might not work
+COMMIT="e901d3e"
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
+CYAN='\033[0;36m'
 NO_COLOR='\033[0m'
 
 STEPS=(
@@ -37,6 +41,7 @@ if [[ ! -d "$DIRECTORY_TO_SAVE_AT/metro-ai-suite" ]]; then
 	sudo git clone -b wdunia-vlm-demo https://github.com/wdunia/edge-ai-suites-prv.git
 	cd edge-ai-suites-prv
 	sudo git sparse-checkout set metro-ai-suite
+	sudo git checkout "$COMMIT"
 	sudo mv "$PARENT_DIRECTORY/edge-ai-suites-prv" "$DIRECTORY_TO_SAVE_AT"
 	echo -e "${GREEN}Cloned repository${NO_COLOR}"
 	statuses[1]="DONE"
@@ -47,8 +52,10 @@ if [[ ! -d "$DIRECTORY_TO_SAVE_AT/metro-ai-suite" ]]; then
 	sudo chmod 777 "$DIRECTORY_TO_SAVE_AT/metro-ai-suite"
 	sudo chmod 777 "$DIRECTORY_TO_SAVE_AT/metro-ai-suite/live-video-analysis"
 	sudo chmod 777 "$DIRECTORY_TO_SAVE_AT/metro-ai-suite/live-video-analysis/showroom-demo"
+	sudo mkdir "$DIRECTORY_TO_SAVE_AT/metro-ai-suite/live-video-analysis/showroom-demo/videos"
+	sudo chmod 777 "$DIRECTORY_TO_SAVE_AT/metro-ai-suite/live-video-analysis/showroom-demo/videos"
 else
-	echo "Directory already exists, skipping repository download"
+	echo -e "${GREEN}Directory already exists, skipping repository download${NO_COLOR}"
 	statuses[1]="EXISTED_BEFORE"
 fi
 cd "$DIRECTORY_TO_SAVE_AT"
@@ -57,7 +64,7 @@ cd metro-ai-suite/live-video-analysis
 # Find wifi interface name
 WIFI_INTERFACE=$(nmcli -t -f DEVICE,TYPE device | awk -F: '$2=="wifi"{print $1; exit}')
 if [ -z "$WIFI_INTERFACE" ]; then
-	echo "No Wi-Fi interface found, skipping network configuration"
+	echo -e "${RED}No Wi-Fi interface found, skipping network configuration${NO_COLOR}"
 	statuses[2]="FAILED"
 else
 	# Create an access point
@@ -79,7 +86,7 @@ else
    	 ipv4.method shared \
    	 ipv4.addresses 192.168.100.1/24
 
-	echo "Added network IntelDemoWLAN on interface: '$WIFI_INTERFACE'"
+	echo "${GREEN}Added network IntelDemoWLAN on interface: '$WIFI_INTERFACE' ${NO_COLOR}"
 	statuses[2]="DONE"
 fi
 
@@ -89,7 +96,7 @@ sudo apt install openssh-server
 sudo systemctl restart ssh.service
 sudo systemctl start ssh.service
 
-echo "Installed and started openssh-server"
+echo -e "${GREEN}Installed and started openssh-server ${NO_COLOR}"
 statuses[3]="DONE"
 
 # Install project dependenices
@@ -104,7 +111,7 @@ if ! id $SSHUSER &>/dev/null; then
 	sudo useradd -m -s /bin/bash $SSHUSER
 	statuses[5]="DONE"
 else
-	echo "User $SSHUSER already added"
+	echo -e "${GREEN}User $SSHUSER already added${NO_COLOR}"
 	statuses[5]="EXISTED_BEFORE"
 fi
 sudo usermod -aG video $SSHUSER
@@ -137,7 +144,7 @@ EOF
 
 		sudo chmod +x "$SSHUSER_HOME/Desktop/RunDemo.desktop"
 		sudo chown "$SSHUSER:$SSHUSER" "$SSHUSER_HOME/Desktop/RunDemo.desktop" 
-		sudo gio set "$SSHUSER_HOME/Desktop/RunDemo.desktop" metadata::trusted true
+		sudo gio set "$SSHUSER_HOME/Desktop/RunDemo.desktop" metadata::trusted yes
 		statuses[6]="DONE"
 	fi
 fi
@@ -160,3 +167,7 @@ for i in "${!STEPS[@]}"; do
 	
 	echo -e "${COLOR}${statuses[$i]}${NO_COLOR} - ${STEPS[$i]}"
 done
+
+echo -e "${CYAN}Script to run demo is at: $DIRECTORY_TO_SAVE_AT/metro-ai-suite/live-video-analysis/showroom-demo/run-demo-alert.sh${NO_COLOR}"
+echo -e "${RED}If this is going to be used in production run the script above at least once. Otherwise it may run for a very long time on the end users device which we don't want. I recommend running it once so the required files install. The following runs will be faster${NO_COLOR}"
+echo -e "${CYAN}To add videos, add them to the $DIRECTORY_TO_SAVE_AT/metro-ai-suite/live-video-analysis/showroom-demo/videos directory${NO_COLOR}"
